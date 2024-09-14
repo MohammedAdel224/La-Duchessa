4f8298bf6ab048eb7f6fe65c6cc35a890a822565
Mohammed Adel <MohammedAdel224@users.noreply.github.com>
2024-09-14 18:38:29 +0300

    implement GetImagesForCart in cart.service.ts

--- src/app/Services/cart.service.ts
+++ src/app/Services/cart.service.ts
@@ -1,28 +1,43 @@
 // src/app/services/cart.service.ts
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { Product } from '../model/product.model'; // Adjust path if needed
+import { BehaviorSubject, combineLatest, map, Observable } from 'rxjs';
+import { CartProduct, OrderProduct, Product } from '../model/product.model'; // Adjust path if needed
 import { HttpClient } from '@angular/common/http';
-import { Order, OrderProduct } from '../model/order.model';
+import { Order} from '../model/order.model';
+import { ProductService } from './product.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CartService {
-  DB_URL = 'http://localhost:3002/users';
+  DB_URL = 'http://localhost:3000/';
   private cartItems: Product[] = [];
   private cartSubject = new BehaviorSubject<Product[]>([]);
-  constructor(private myHttp: HttpClient) {}
+  constructor(private http: HttpClient, private productServes: ProductService) {}
   cart$ = this.cartSubject.asObservable();
 
   getCartItems(): Product[] {
     return this.cartItems;
   }
   updateCart(id: string, update: { cart: OrderProduct[] }): Observable<any> {
-    return this.myHttp.patch(this.DB_URL + '/' + id, update);
+    return this.http.patch(this.DB_URL + 'users/' + id, update);
   }
 
   MakeOrder(order: Order) {
-    return this.myHttp.post(this.DB_URL, order);
+    return this.http.post(this.DB_URL + 'orders/', order);
+  }
+
+  GetImagesForCart(cart: OrderProduct[]): Observable<CartProduct[]> {
+    const products$ = this.productServes.getProducts();
+    const cart$ = new BehaviorSubject<OrderProduct[]>(cart).asObservable();
+
+    return combineLatest([cart$, products$]).pipe(
+      map(([cart, products]) => {
+        return cart.map((cartProduct: OrderProduct) => ({
+          ...cartProduct,
+          image: products.find((product: Product) => product.id === cartProduct.id)!.image
+        }));
+      })
+    );
   }
 }
