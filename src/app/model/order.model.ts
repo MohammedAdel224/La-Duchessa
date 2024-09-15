39f0064bc53beb40ffbb8199a34691e81de506c8
Mohammed Adel <MohammedAdel224@users.noreply.github.com>
2024-09-15 11:45:48 +0300

    edit order model data types

--- src/app/model/order.model.ts
+++ src/app/model/order.model.ts
@@ -1,10 +1,10 @@
 import { OrderProduct } from "./product.model";
 
 export interface Order {
-  id: string;
+  id?: string;
   userId: string;
-  datetime: string;
+  datetime: string | null;
   products: OrderProduct[];
   total: number;
-  status: string;
+  status: "Pending" | "Accepted" | "Rejected" | "Canceled";
 }
