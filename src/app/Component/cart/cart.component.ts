7f871c3094c098b19ae0cca8ff6eaa04fef58e19
Mohammed Adel <MohammedAdel224@users.noreply.github.com>
2024-09-15 11:46:12 +0300

    Reimplement cart component

--- src/app/Component/cart/cart.component.ts
+++ src/app/Component/cart/cart.component.ts
@@ -1,105 +1,27 @@
-// import { Component, OnInit } from '@angular/core';
-// import { CartService } from '../../Services/cart.service';
-// import { CommonModule } from '@angular/common';
-// import { User } from '../../model/user.model';
-// import { CommonVariablesService } from '../../Services/common-variables.service';
-// import { Order } from '../../model/order.model';
-// import { ActivatedRoute, Router } from '@angular/router';
-// import { ProductService } from '../../Services/product.service';
-// import { FormsModule } from '@angular/forms';
-// import { HttpClientModule } from '@angular/common/http';
-// import { Product } from '../../model/product.model';
-// import { OrderrService } from '../../Services/orderr.service';
-// @Component({
-//   selector: 'app-cart',
-//   standalone: true,
-//   imports: [HttpClientModule, CommonModule, FormsModule],
-//   providers: [ProductService],
-//   templateUrl: './cart.component.html',
-//   styleUrl: './cart.component.css',
-// })
-// export class CartComponent implements OnInit {
-//   userid: string | null = null;
-//   Products: Product[] = [];
-//   user: User = {
-//     id: '',
-//     userType: 'none',
-//     profilePicture: '',
-//     userName: '',
-//     email: '',
-//     gender: '',
-//     address: '',
-//     cart: [],
-//     order: [],
-//   };
-//   constructor(
-//     public commonVariables: CommonVariablesService,
-//     private cartService: CartService,
-//     private orderrservice: OrderrService,
-//     public prodserve: ProductService,
 
-//     public router: Router,
-//     public route: ActivatedRoute
-//   ) {}
-
-//   ngOnInit(): void {
-//     this.commonVariables.user$.subscribe((user: User) => {
-//       this.user = user;
-//       delete this.user.password;
-//     });
-//   }
-
-//   makeorder(order: Order) {
-//     this.cartService.MakeOrder(order);
-//   }
-//   addorder(product: Product) {
-//     // this.cartService.addToCart(product);
-//     this.user.order.push(product);
-//     this.orderrservice.updateorder(this.user.id, { order: this.user.order });
-//   }
-//   deletefromcart(product: Product) {
-//     // Find the index of the product to remove
-//     const productIndex = this.user.cart.findIndex((p) => p.id === product.id);
-
-//     // If the product is found in the cart
-//     if (productIndex !== -1) {
-//       // Remove the product from the cart array
-//       this.user.cart.splice(productIndex, 1);
-
-//       // Update the cart in the backend
-//       this.cartService.updatecart(this.user.id, { cart: this.user.cart });
-//     } else {
-//       console.warn('Product not found in cart');
-//     }
-//   }
-//   vieworders() {
-//     this.router.navigate(['/orderr']);
-//   }
-// }
-// cart.component.ts
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { CartService } from '../../Services/cart.service';
-import { OrderrService } from '../../Services/orderr.service';
 import { CommonVariablesService } from '../../Services/common-variables.service';
 import { User } from '../../model/user.model';
-import { Product } from '../../model/product.model';
-import { Order } from '../../model/order.model';
 import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { ProductService } from '../../Services/product.service';
+import { CartProduct, OrderProduct } from '../../model/product.model';
+import { Order } from '../../model/order.model';
+import { OrderService } from '../../Services/order.service';
+import { identifierName } from '@angular/compiler';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
   imports: [HttpClientModule, CommonModule, FormsModule],
-  providers: [ProductService],
+  providers: [DatePipe],
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
-  userid: string | null = null;
+  cart: CartProduct[] = [];
   user: User = {
     id: '',
     userType: 'none',
@@ -113,10 +35,10 @@ export class CartComponent implements OnInit {
   };
 
   constructor(
-    public commonVariables: CommonVariablesService,
+    private commonVariables: CommonVariablesService,
     private cartService: CartService,
-    private orderrservice: OrderrService,
-    public router: Router
+    private orderService: OrderService,
+    private datePipe: DatePipe
   ) {}
 
   ngOnInit(): void {
@@ -124,101 +46,60 @@ export class CartComponent implements OnInit {
       this.user = user;
       delete this.user.password;
     });
-  }
-  goBack(): void {
-    this.router.navigate(['/products']);
-  }
-  makeorder(product: any) {
-    if (this.user.id) {
-      // Add the product to the user's cart
-      this.user.order.push(product);
 
-      // Update the cart on the server
-      this.cartService
-        .updateCart(this.user.id, { cart: this.user.cart })
-        .subscribe({
-          next: (response: any) => {
-            console.log('Cart updated successfully:', response);
-          },
-        });
+    this.cartService.GetImagesForCart(this.user.cart).subscribe({
+      next: (cart)=>this.cart = cart
+    });
+  }
+
+  increaseQuantity(productId: string | null){
+    var cartProduct = this.cart.filter((product: CartProduct)=> product.id === productId)[0];
+    cartProduct.quantity += 1;
+
+    var userCartProduct = this.user.cart.filter((product: OrderProduct)=> product.id === productId)[0];
+    userCartProduct.quantity += 1;
+
+    this.cartService.updateCart(this.user.id, {cart: this.user.cart}).subscribe();
+  }
+
+  decreaseQuantity(productId: string | null){
+    var cartProduct = this.cart.filter((product: CartProduct)=> product.id === productId)[0];
+    cartProduct.quantity = Math.max(1, cartProduct.quantity - 1);
+
+    var userCartProduct = this.user.cart.filter((product: OrderProduct)=> product.id === productId)[0];
+    userCartProduct.quantity = Math.max(1, userCartProduct.quantity - 1);
+
+    this.cartService.updateCart(this.user.id, {cart: this.user.cart}).subscribe();
+  }
+
+  removeFromCart(productId: string | null){
+    this.cart = this.cart.filter((pruduct)=> pruduct.id !== productId);
+    this.user.cart = this.user.cart.filter((pruduct)=> pruduct.id !== productId);
+    this.cartService.updateCart(this.user.id, {cart: this.user.cart}).subscribe();
+  }
+
+  getFormatedDate(date: Date){
+    return this.datePipe.transform(date, 'MMMM dd, yyyy hh:mm:ss a')
+  }
+
+  makeOrder() {
+    let total = 0;
+    for(let product of this.user.cart){
+      total += product.quantity * product.price;
     }
-  }
-  // addorder() {
-  //   if (this.user.id) {
-  //     // Add the product to the user's cart
-  //     this.user.order.push(this.user.cart);
-  //     // Update the cart on the server
-  //     this.cartService
-  //       .updateCart(this.user.id, { cart: this.user.cart })
-  //       .subscribe({
-  //         next: (response: any) => {
-  //           console.log('Cart updated successfully:', response);
-  //         },
-  //       });
-  //   }
-
-  //   if (this.user.id && this.user.cart.length > 0) {
-  //     // Calculate total
-  //     const total = this.user.cart.reduce(
-  //       (acc, product) => acc + (product.price || 0),
-  //       0
-  //     );
-
-  //     // Create order object
-  //     const order: Order = {
-  //       id: this.generateOrderId(), // Generate a unique order ID
-  //       userId: this.user.id,
-  //       datetime: new Date().toISOString(), // Current date and time
-  //       products: this.user.cart,
-  //       total: total,
-  //       status: 'Pending', // Default status
-  //     };
-
-  //     // Create the order in the backend
-  //     this.orderrservice.createOrder(order).subscribe({
-  //       next: () => {
-  //         console.log('Order added successfully');
-
-  //         // Clear the cart
-  //         this.user.cart = [];
-  //         this.cartService
-  //           .updatecart(this.user.id, { cart: this.user.cart })
-  //           .subscribe({
-  //             next: () => console.log('Cart cleared successfully'),
-  //             error: (error) => console.error('Error clearing cart:', error),
-  //           });
-  //       },
-  //       error: (error) => {
-  //         console.error('Error adding order:', error);
-  //       },
-  //     });
-  //   } else {
-  //     console.error('User ID not found or cart is empty.');
-  //   }
-  // }
-
-  generateOrderId(): string {
-    // Generate a unique order ID
-    return 'order-' + Math.random().toString(36).substr(2, 9);
-  }
-
-  // deletefromcart(product: Product) {
-  //   const productIndex = this.user.cart.findIndex((p) => p.id === product.id);
-  //   if (productIndex !== -1) {
-  //     this.user.cart.splice(productIndex, 1);
-  //     this.cartService
-  //       .updatecart(this.user.id, { cart: this.user.cart })
-  //       .subscribe({
-  //         next: () => console.log('Product removed from cart'),
-  //         error: (error) =>
-  //           console.error('Error removing product from cart:', error),
-  //       });
-  //   } else {
-  //     console.warn('Product not found in cart');
-  //   }
-  // }
-
-  vieworders() {
-    this.router.navigate(['/orderr']);
+    const order: Order = {
+      userId: this.user.id,
+      datetime: this.getFormatedDate(new Date()),
+      products: this.user.cart,
+      total: total,
+      status: "Pending"
+    }
+    this.orderService.AddOrders(order).subscribe({
+      next: ()=>{
+        this.cart = [];
+        this.user.cart = [];
+        this.cartService.updateCart(this.user.id, { cart: [] });
+      }
+    })
   }
 }
